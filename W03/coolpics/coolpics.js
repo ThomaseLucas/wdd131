9f50c585af938ae4f7059fae6a3e97d784bd161f
Thomaselucas <thomaselucas2020@gmail.com>
2025-05-19 14:09:24 -0600

    finished week 4 assignmnet

--- W03/coolpics/coolpics.js
+++ W03/coolpics/coolpics.js
@@ -1,4 +1,4 @@
-const button = document.querySelector('button');
+const button = document.querySelector('#menu-button');
 const nav_links = document.querySelectorAll('nav a');
 
 button.addEventListener('click', function() {
@@ -23,7 +23,40 @@ window.addEventListener('resize', function(){
         });
     } else {
         nav_links.forEach(function (nav) {
-            nav.style.display = 'none';
+            nav.style.display = 'none'; 
         });
     }
 });
+
+const gallery = document.querySelector('.gallery');
+const modal = document.querySelector('#modal-viewer');
+const modalImage = document.querySelector('#modal-image');
+const close_button = document.querySelector('.close-viewer');
+
+gallery.addEventListener('click', (event) => {
+    const clickedImage = event.target.closest('img');
+
+    if (!clickedImage) return;
+    
+    const originalSrc = clickedImage.src;
+    const baseName = originalSrc.split('-')[0];
+    const fullImage = baseName + '-full.jpeg';
+
+    modalImage.src = fullImage;
+    modal.showModal();
+})
+
+close_button.addEventListener('click', () =>{
+    console.log('This is working!');
+    modal.close();
+})
+
+modal.addEventListener('click', (event) => {
+    if (event.target == modal) {
+        modal.close();
+    }
+})
+
+
+
+
